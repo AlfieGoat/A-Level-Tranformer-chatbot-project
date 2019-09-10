960bf9d1133404ced2b3e1b416d797c1d840bffd
Alfie <alfred17686@gmail.com>
2019-09-11 00:29:07 +0100

    Removed batching and finished up tally

--- scripts/pre_processing_token_dict_creator.py
+++ scripts/pre_processing_token_dict_creator.py
@@ -1,25 +1,26 @@
 import json
 import time
+import pickle
+
 
 class TokenDictCreator:
 
     def create_dict(self, file_paths, token_dict_save_path,
                     vocab_size, filter_parameters=(2, ["deleted", "removed"]),
-                    track_progress=50000, batch_size=10000000):
+                    track_progress=2000000):
         # track_progress is the num of rows to process before
         # giving elapsed time. Set to 0 for no tracking
         # filter_parameters will be a tuple with the first index
         # being the min upvotes, second index being list of invalid
         # words
 
-        tally_dict = self.count_tokens(file_paths, filter_parameters, track_progress, batch_size)
+        tally_dict = self.count_tokens(file_paths, filter_parameters, track_progress)
 
     @staticmethod
-    def count_tokens(file_paths, filter_parameters,
-                     track_progress=50000, batch_size=10000000):
+    def count_tokens(file_paths, filter_parameters=(2, ["deleted", "removed"]),
+                     track_progress=200000):
 
         punctuation = '!"£$%^&*()_+={[}]:;@~#<,>.?/'
-        batch = []
         counting_dict = {}
         if track_progress > 0:
             start_time = time.time()
@@ -31,36 +32,35 @@ class TokenDictCreator:
                 print(f"Processing: {file_path}")
 
             for line_num, line in enumerate(json_file):
-                batch.append(line)
 
-                if (track_progress > 0) and ((line_num % track_progress) == 0):
-                    print(f"Time elapsed: {round(start_time - time.time(), 2)} seconds.")
+                if (track_progress > 0) and ((line_num % track_progress) == 0) and line_num != 0:
+                    print(f"\nTime elapsed: {round(time.time() - start_time, 2)} seconds.")
+                    print(f"Lines processed: {line_num}")
 
-                if (line_num % batch_size) == 0 and line_num != 0:
+                current_line_converted = json.loads(line)
 
-                    for batch_line in batch:
-                        current_line_converted = json.loads(batch_line)
+                if int(current_line_converted["score"]) >= filter_parameters[0]:
+                    body = current_line_converted["body"]
 
-                        if int(current_line_converted["score"]) >= filter_parameters[0]:
-                            body = current_line_converted["body"]
+                    for i in punctuation:
+                        body = body.replace(i, f" {i} ")
 
-                            for i in punctuation:
-                                body = body.replace(i, f" {i} ")
+                    body = body.split()
 
-                            body = body.split()
+                    flag = False
+                    for banned_token in filter_parameters[1]:
+                        if banned_token in body:
+                            flag = True
 
-                            for banned_tokens in filter_parameters[1]:
-                                if banned_tokens in current_line_converted:
-                                    break
+                    if not flag:
+                        for token in body:
 
-                            for token in body:
+                            if token not in counting_dict:
+                                counting_dict[token] = 1
 
-                                if token not in counting_dict:
-                                    counting_dict[token] = 0
+                            else:
+                                counting_dict[token] = counting_dict[token] + 1
 
-                                else:
-                                    counting_dict[token] = counting_dict[token] + 1
-                    batch = []
 
         return counting_dict
 
--- scripts/test_script.py
+++ scripts/test_script.py
@@ -1,2 +1,6 @@
 import pre_processing_token_dict_creator
-print("test")
+import pickle
+
+token_dict = pre_processing_token_dict_creator.TokenDictCreator()
+pickle.dump(token_dict.count_tokens(["Z:/Code/A-Level-Tranformer-chatbot-project/data/uncompressed/RC_2019-05"]),
+            open("tally_dict.pickle", "wb"))
